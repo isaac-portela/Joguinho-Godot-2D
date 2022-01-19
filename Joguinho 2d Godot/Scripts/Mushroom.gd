4380562c5cb589b17f4f19fe8023c4de24282208
Isaac Portela <isaacportela2003@gmail.com>
2022-01-19 20:13:19 -0300

    Códigos do jogo da aula 8 a 12 feitos

---
+++ Joguinho 2d Godot/Scripts/Mushroom.gd
@@ -0,0 +1,52 @@
+extends KinematicBody2D
+
+export var speed = 32
+export var health = 1
+var motion = Vector2.ZERO
+var move_direction = -1
+var gravity = 1200
+var hitted = false
+
+func _physics_process(delta):
+	motion.x = speed * move_direction
+	motion.y += gravity * delta
+
+	if move_direction == 1:
+		$texture.flip_h = true
+	else:
+		$texture.flip_h = false
+	
+	_set_animation()
+	
+	motion = move_and_slide(motion)
+
+
+func _on_anim_animation_finished(anim_name):
+	if anim_name == "idle":
+		$ray_wall.scale *= -1
+		move_direction *= -1
+		$anim.play("run")
+
+func _set_animation():
+	var anim = "run"
+	
+	if $ray_wall.is_colliding():
+		anim = "idle"
+	elif motion.x != 0:
+		anim = "run"
+	if hitted == true:
+		anim = "hit"
+		
+	if $anim.assigned_animation != anim:
+		$anim.play(anim)
+
+
+func _on_hitbox_body_entered(body):
+	hitted = true
+	health -= 1
+	body.velocity.y -= 300
+	yield(get_tree().create_timer(0.2), "timeout")
+	hitted = false
+	if health < 1:
+		queue_free()
+		get_node("hitbox/collision").set_deferred("disabled", true)
--- Joguinho 2d Godot/Scripts/Player.gd
+++ Joguinho 2d Godot/Scripts/Player.gd
@@ -1,33 +1,45 @@
 extends KinematicBody2D
 
+var UP = Vector2.UP
 var velocity = Vector2.ZERO
 var move_speed = 480
 var gravity = 1200
 var jump_force = -720 
 var is_grounded
+var health = 3
+var hurted = false
+var knockback_dir = 1
+var knockback_int = 500
 onready var raycasts = $raycasts
 
-func _physics_process(delta:float) -> void:
+func _physics_process(delta:float):
 	velocity.y += gravity * delta
+	velocity.x = 0
 	
-	_get_input()
+	if !hurted:
+		_get_input()
 	
-	velocity = move_and_slide(velocity)
+	velocity = move_and_slide(velocity, UP)
 	
 	is_grounded = _check_is_ground()
 	
 	_set_animation()
+	
+	for platforms in get_slide_count():
+		var collision = get_slide_collision(platforms)
+		if collision.collider.has_method("collid_with"):
+			collision.collider.collid_with(collision, self)
 	
 func _get_input():
 	velocity.x = 0
 	var move_direction = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
 	velocity.x = lerp(velocity.x, move_speed * move_direction, 0.2)
 	
-	if move_direction !=0:
+	if move_direction != 0:
 		$texture.scale.x = move_direction	
-
-func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("jump") && is_grounded:
+		knockback_dir = move_direction
+func _input(event: InputEvent):
+	if event.is_action_pressed("jump") and is_grounded:
 		velocity.y = jump_force / 2
 
 func _check_is_ground():
@@ -43,6 +55,26 @@ func _set_animation():
 		anim = "jump"
 	elif velocity.x != 0:
 		anim = "run"
+	if velocity.y > 0 and !is_grounded:
+		anim = "fall"
+	if hurted:
+		anim = "hit"
 		
 	if $anim.assigned_animation != anim:
 		$anim.play(anim)
+	
+func knockback():
+	velocity.x =  -knockback_dir * knockback_int
+	velocity = move_and_slide(velocity)
+	
+func _on_hurtbox_body_entered(body):
+	health -= 1
+	hurted = true
+	knockback()
+	get_node("hurtbox/collision").set_deferred("disabled", true)
+	yield(get_tree().create_timer(0.5), "timeout")
+	get_node("hurtbox/collision").set_deferred("disabled", false)
+	hurted = false
+	if health < 1:
+		queue_free()
+		get_tree().reload_current_scene()
